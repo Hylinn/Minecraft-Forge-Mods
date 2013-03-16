4c10350a70cc6e74265390d2d180b9704ecde3da
Hylinn <hylinn.taggart+git@gmail.com>
2013-03-16 16:51:13 -0400

    Removed unused imports.

--- src/mods/hylinn/minecraft/ElementalWands/item/ItemWand.java
+++ src/mods/hylinn/minecraft/ElementalWands/item/ItemWand.java
@@ -5,21 +5,15 @@ import hylinn.minecraft.ElementalWands.event.WandCastEvent;
 import hylinn.minecraft.ElementalWands.event.WandChargeEvent;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
-import net.minecraft.block.Block;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.enchantment.Enchantment;
-import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.projectile.EntityArrow;
 import net.minecraft.item.EnumAction;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Icon;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.player.ArrowLooseEvent;
-import net.minecraftforge.event.entity.player.ArrowNockEvent;
 
 public class ItemWand extends Item {
 	
