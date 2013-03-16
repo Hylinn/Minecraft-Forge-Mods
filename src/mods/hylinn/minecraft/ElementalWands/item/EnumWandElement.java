47b09121068bea1dcebd66776a0c3e2187409db8
Hylinn <hylinn.taggart+git@gmail.com>
2013-03-16 00:25:09 -0400

    Added method getElementCraftingMaterial

--- src/mods/hylinn/minecraft/ElementalWands/item/EnumWandElement.java
+++ src/mods/hylinn/minecraft/ElementalWands/item/EnumWandElement.java
@@ -1,9 +1,26 @@
 package hylinn.minecraft.ElementalWands.item;
 
+import net.minecraft.item.Item;
+
 public enum EnumWandElement {
 	FIRE,
 	EARTH,
 	WATER,
 	AIR,
 	ARCANE;
+	
+	public Item customCraftingMaterial = null;
+	
+	public int getElementCraftingMaterial()
+    {
+        switch (this)
+        {
+            case FIRE:   return Item.blazeRod.itemID;
+            case EARTH:  return Item.emerald.itemID;
+            case WATER:  return Item.ghastTear.itemID;
+            case AIR:    return Item.enderPearl.itemID;
+            case ARCANE: return Item.lightStoneDust.itemID;
+            default:     return (customCraftingMaterial == null ? 0 : customCraftingMaterial.itemID);
+        }
+    }
 }
