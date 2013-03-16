1019760bdd36b98dd3604df01c302997cc2f7128
Hylinn <hylinn.taggart+git@gmail.com>
2013-03-15 21:04:39 -0400

    Fleshed out EnumWandMaterial a bit.

--- src/mods/hylinn/minecraft/ElementalWands/item/EnumWandMaterial.java
+++ src/mods/hylinn/minecraft/ElementalWands/item/EnumWandMaterial.java
@@ -1,9 +1,44 @@
 package hylinn.minecraft.ElementalWands.item;
 
+import net.minecraft.item.Item;
+
 public enum EnumWandMaterial {
-	WOOD,
-	BONE,
-	IRON,
-	QUARTZ,
-	GOLD;
+	WOOD   (10, 10),
+	BONE   (20, 15),
+	IRON   (30, 12),
+	QUARTZ (60, 12),
+	GOLD   (50, 20);
+	
+	private final int enchantability;
+	private final int maxCharges;
+	
+	public Item customCraftingMaterial = null;
+	
+	private EnumWandMaterial(int maxCharges, int enchantability) {
+		this.enchantability = enchantability;
+		this.maxCharges = maxCharges;
+	}
+	
+	public int getMaxCharges()
+    {
+        return this.maxCharges;
+    }
+	
+	public int getEnchantability()
+    {
+        return this.enchantability;
+    }
+	
+	public int getToolCraftingMaterial()
+    {
+        switch (this)
+        {
+            case WOOD:    return Item.stick.itemID;
+            case BONE:    return Item.bone.itemID;
+            case GOLD:    return Item.ingotGold.itemID;
+            case IRON:    return Item.ingotIron.itemID;
+            case QUARTZ:  return Item.field_94583_ca.itemID; // field_94583_ca will probably change in the future.
+            default:      return (customCraftingMaterial == null ? 0 : customCraftingMaterial.itemID);
+        }
+    }
 }
