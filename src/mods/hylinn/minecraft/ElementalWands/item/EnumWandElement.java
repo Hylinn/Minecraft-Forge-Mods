df62ef100fe2b2a77b9c05c6ba2a227f056a7102
Hylinn <hylinn.taggart+git@gmail.com>
2013-03-16 14:35:28 -0400

    Added name and toString to enums.

--- src/mods/hylinn/minecraft/ElementalWands/item/EnumWandElement.java
+++ src/mods/hylinn/minecraft/ElementalWands/item/EnumWandElement.java
@@ -4,20 +4,23 @@ import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 
 public enum EnumWandElement {
-	FIRE   (Item.blazeRod),
-	EARTH  (Item.emerald),
-	WATER  (Item.ghastTear),
-	AIR    (Item.enderPearl),
-	ARCANE (Item.lightStoneDust);
+	FIRE   (Item.blazeRod, "Fire"),
+	EARTH  (Item.emerald, "Earth"),
+	WATER  (Item.ghastTear, "Water"),
+	AIR    (Item.enderPearl, "Air"),
+	ARCANE (Item.lightStoneDust, "Arcane");
 	
 	private final Object material;
+	private final String name;
 	
-	private EnumWandElement(Item material) {
+	private EnumWandElement(Item material, String name) {
 		this.material = material;
+		this.name = name;
 	}
 	
-	private EnumWandElement(Block material) {
+	private EnumWandElement(Block material, String name) {
 		this.material = material;
+		this.name = name;
 	}
 	
 	public Object getMaterial() {
@@ -26,5 +29,9 @@ public enum EnumWandElement {
 	
 	public int getMaterialID() {
 		return material instanceof Item ? ((Item) material).itemID : ((Block) material).blockID;
+	}
+	
+	public String toString() {
+		return this.name;
 	}
 }
--- src/mods/hylinn/minecraft/ElementalWands/item/EnumWandMaterial.java
+++ src/mods/hylinn/minecraft/ElementalWands/item/EnumWandMaterial.java
@@ -4,26 +4,29 @@ import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 
 public enum EnumWandMaterial {
-	WOOD   (10, 10, Item.stick),
-	BONE   (20, 15, Item.bone),
-	IRON   (30, 12, Item.ingotIron),
-	QUARTZ (60, 12, Item.field_94583_ca),
-	GOLD   (50, 20, Item.ingotGold);
+	WOOD   (10, 10, Item.stick, "Wood"),
+	BONE   (20, 15, Item.bone, "Bone"),
+	IRON   (30, 12, Item.ingotIron, "Iron"),
+	QUARTZ (60, 12, Item.field_94583_ca, "Quartz"),
+	GOLD   (50, 20, Item.ingotGold, "Gold");
 	
 	private final int enchantability;
 	private final int maxCharges;
 	private final Object material;
+	private final String name;
 	
-	private EnumWandMaterial(int maxCharges, int enchantability, Item material) {
+	private EnumWandMaterial(int maxCharges, int enchantability, Item material, String name) {
 		this.enchantability = enchantability;
 		this.maxCharges = maxCharges;
 		this.material = material;
+		this.name = name;
 	}
 	
-	private EnumWandMaterial(int maxCharges, int enchantability, Block material) {
+	private EnumWandMaterial(int maxCharges, int enchantability, Block material, String name) {
 		this.enchantability = enchantability;
 		this.maxCharges = maxCharges;
 		this.material = material;
+		this.name = name;
 	}
 	
 	public int getMaxCharges()
@@ -42,5 +45,9 @@ public enum EnumWandMaterial {
 	
 	public int getMaterialID() {
 		return material instanceof Item ? ((Item) material).itemID : ((Block) material).blockID;
+	}
+	
+	public String toString() {
+		return this.name;
 	}
 }
