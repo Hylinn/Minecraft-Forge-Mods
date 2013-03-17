571b1c4b4f19ec0846b66d1fe5973ecbab5a280a
Hylinn <hylinn.taggart+git@gmail.com>
2013-03-16 23:31:00 -0400

    Added and modified enchants.

--- src/mods/hylinn/minecraft/ElementalWands/enchantment/EnchantmentFireShield.java
+++ src/mods/hylinn/minecraft/ElementalWands/enchantment/EnchantmentFireShield.java
@@ -1,24 +1,24 @@
 package hylinn.minecraft.ElementalWands.enchantment;
 
+import hylinn.minecraft.ElementalWands.item.EnumWandElement;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
-import hylinn.minecraft.ElementalWands.item.EnumWandElement;
 
-public class EnchantmentFireball extends EnchantmentWand implements ICastable{
+public class EnchantmentFireShield extends EnchantmentWand implements ICastable {
 
 	protected final int MIN_LEVEL = 1;
 	protected final int MAX_LEVEL = 5;
-	private final String enchantmentName = "Fireball";
+	private final String enchantmentName = "Fire Shield";
 	
-	public EnchantmentFireball(int id, int weight) {
+	public EnchantmentFireShield(int id, int weight) {
 		super(id, weight, EnumWandElement.FIRE);
-		this.setName("fireball");
+		this.setName("fireShield");
 	}
 
 	public int cast(ItemStack stack, World world, EntityLiving entity, int level, int charge) {
-		System.out.println("Fireball Cast");
+		System.out.println("Fire Shield Cast");
 		return 1;
 	}
 	
@@ -33,4 +33,5 @@ public class EnchantmentFireball extends EnchantmentWand implements ICastable{
 	public String getTranslatedName(int level) {
 		return this.enchantmentName + " " + StatCollector.translateToLocal("enchantment.level." + level);
 	}
+
 }
--- src/mods/hylinn/minecraft/ElementalWands/enchantment/EnchantmentFlameWall.java
+++ src/mods/hylinn/minecraft/ElementalWands/enchantment/EnchantmentFlameWall.java
@@ -1,24 +1,24 @@
 package hylinn.minecraft.ElementalWands.enchantment;
 
+import hylinn.minecraft.ElementalWands.item.EnumWandElement;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
-import hylinn.minecraft.ElementalWands.item.EnumWandElement;
 
-public class EnchantmentFireball extends EnchantmentWand implements ICastable{
+public class EnchantmentFlameWall extends EnchantmentWand implements ICastable {
 
 	protected final int MIN_LEVEL = 1;
 	protected final int MAX_LEVEL = 5;
-	private final String enchantmentName = "Fireball";
+	private final String enchantmentName = "Flame Wall";
 	
-	public EnchantmentFireball(int id, int weight) {
+	public EnchantmentFlameWall(int id, int weight) {
 		super(id, weight, EnumWandElement.FIRE);
 		this.setName("fireball");
 	}
 
 	public int cast(ItemStack stack, World world, EntityLiving entity, int level, int charge) {
-		System.out.println("Fireball Cast");
+		System.out.println("Flame Wall Cast");
 		return 1;
 	}
 	
@@ -33,4 +33,5 @@ public class EnchantmentFireball extends EnchantmentWand implements ICastable{
 	public String getTranslatedName(int level) {
 		return this.enchantmentName + " " + StatCollector.translateToLocal("enchantment.level." + level);
 	}
+
 }
