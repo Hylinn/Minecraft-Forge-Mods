0194bec3e30e3349bcf612d7a53a170bd5ae5d5a
Hylinn <hylinn.taggart+git@gmail.com>
2013-03-17 13:41:18 -0400

    Corrected spelling error.

--- src/mods/hylinn/minecraft/ElementalWands/enchantment/EnchantmentFlameStep.java
+++ src/mods/hylinn/minecraft/ElementalWands/enchantment/EnchantmentFlameStep.java
@@ -3,14 +3,13 @@ package hylinn.minecraft.ElementalWands.enchantment;
 import hylinn.minecraft.ElementalWands.item.EnumWandElement;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
 
 public class EnchantmentFlameStep extends EnchantmentWand {
 
 	public EnchantmentFlameStep(int id, int weight) {
 		super(id, weight, EnumWandElement.FIRE);
-		this.setName("falmeStep");
+		this.setName("flameStep");
 	}
 
 	public int cast(ItemStack stack, World world, EntityLiving entity, int level, int itemInUseDuration) {
