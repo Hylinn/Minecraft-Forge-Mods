db9490a1839e279b32f1d4be9bea231861980110
Hylinn <hylinn.taggart+git@gmail.com>
2013-03-17 13:36:06 -0400

    Added localization support

--- src/mods/hylinn/minecraft/ElementalWands/ElementalWands.java
+++ src/mods/hylinn/minecraft/ElementalWands/ElementalWands.java
@@ -1,5 +1,11 @@
 package hylinn.minecraft.ElementalWands;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnumEnchantmentType;
@@ -48,46 +54,47 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 
-@Mod(modid="ElementalWands", name="Elemental Wands", version="0.0.2")
+@Mod(modid="ElementalWands", name="Elemental Wands", version="0.0.3")
 @NetworkMod(clientSideRequired=true, serverSideRequired=false)
 public class ElementalWands {
 
     public static ItemWand[] wands;
     private static final int wandStartId = 31000;
     public static final String modName = "ElementalWands";
+    private static final String languageDirectory = "resources/mods/" + modName + "/localization/";
     
     public static final EnumEnchantmentType enchantmentWand = EnumHelper.addEnchantmentType("wand");
     public static final EnumAction actionWand = EnumHelper.addAction("wand");
     
-    public static final Enchantment fireball = new EnchantmentFireball(200, 10);
-    public static final Enchantment flameStep = new EnchantmentFlameStep(201, 10);
-    public static final Enchantment flameWall = new EnchantmentFlameWall(202, 10);
-    public static final Enchantment fireShield = new EnchantmentFireShield(203, 10);
-    public static final Enchantment flameWeapon = new EnchantmentFlameWeapon(204, 10);
+    public static final Enchantment fireball    = new EnchantmentFireball   (200, 3);
+    public static final Enchantment flameStep   = new EnchantmentFlameStep  (201, 3);
+    public static final Enchantment flameWall   = new EnchantmentFlameWall  (202, 3);
+    public static final Enchantment fireShield  = new EnchantmentFireShield (203, 3);
+    public static final Enchantment flameWeapon = new EnchantmentFlameWeapon(204, 3);
     
-    public static final Enchantment toughness = new EnchantmentToughness(205, 10);
-    public static final Enchantment light = new EnchantmentLight(206, 10);
-    public static final Enchantment dispel = new EnchantmentDispel(207, 10);
-    public static final Enchantment magicMissile = new EnchantmentMagicMissile(208, 10);
-    public static final Enchantment arcaneShield = new EnchantmentArcaneShield(209, 10);
+    public static final Enchantment toughness 	 = new EnchantmentToughness   (205, 3);
+    public static final Enchantment light 		 = new EnchantmentLight		  (206, 3);
+    public static final Enchantment dispel 		 = new EnchantmentDispel	  (207, 3);
+    public static final Enchantment magicMissile = new EnchantmentMagicMissile(208, 3);
+    public static final Enchantment arcaneShield = new EnchantmentArcaneShield(209, 3);
     
-    public static final Enchantment gust = new EnchantmentGust(210, 10);
-    public static final Enchantment airShield = new EnchantmentAirShield(211, 10);
-    public static final Enchantment safeFall = new EnchantmentSafeFall(212, 10);
-    public static final Enchantment vortex = new EnchantmentVortex(213, 10);
-    public static final Enchantment lightning = new EnchantmentLightning(214, 10);
+    public static final Enchantment gust 	  = new EnchantmentGust		(210, 3);
+    public static final Enchantment airShield = new EnchantmentAirShield(211, 3);
+    public static final Enchantment safeFall  = new EnchantmentSafeFall (212, 3);
+    public static final Enchantment vortex 	  = new EnchantmentVortex	(213, 3);
+    public static final Enchantment lightning = new EnchantmentLightning(214, 3);
     
-    public static final Enchantment stoneSpikes = new EnchantmentStoneSpikes(215, 10);
-    public static final Enchantment detectOre = new EnchantmentDetectOre(216, 10);
-    public static final Enchantment entangle = new EnchantmentEntangle(217, 10);
-    public static final Enchantment earthShield = new EnchantmentEarthShield(218, 10);
-    public static final Enchantment burrow = new EnchantmentBurrow(219, 10);
+    public static final Enchantment stoneSpikes = new EnchantmentStoneSpikes(215, 3);
+    public static final Enchantment detectOre   = new EnchantmentDetectOre	(216, 3);
+    public static final Enchantment entangle 	= new EnchantmentEntangle	(217, 3);
+    public static final Enchantment earthShield = new EnchantmentEarthShield(218, 3);
+    public static final Enchantment burrow 		= new EnchantmentBurrow		(219, 3);
     
-    public static final Enchantment iceBolt = new EnchantmentIceBolt(220, 10);
-    public static final Enchantment swiftSwim = new EnchantmentSwiftSwim(221, 10);
-    public static final Enchantment conjureWater = new EnchantmentConjureWater(222, 10);
-    public static final Enchantment iceShield = new EnchantmentIceShield(223, 10);
-    public static final Enchantment summonSnowGolem = new EnchantmentSummonSnowGolem(224, 10);
+    public static final Enchantment iceBolt 		= new EnchantmentIceBolt		(220, 3);
+    public static final Enchantment swiftSwim 		= new EnchantmentSwiftSwim		(221, 3);
+    public static final Enchantment conjureWater 	= new EnchantmentConjureWater	(222, 3);
+    public static final Enchantment iceShield 		= new EnchantmentIceShield		(223, 3);
+    public static final Enchantment summonSnowGolem = new EnchantmentSummonSnowGolem(224, 3);
     
     public static final CreativeTabs wandTab = new CreativeTabWand("wandTab");
     
@@ -108,6 +115,20 @@ public class ElementalWands {
     	//event.getModMetadata().credits = "";
     	//event.getModMetadata().updateUrl = "";
     	//event.getModMetadata().modId = "";
+    	
+    	// Register language packs.
+    	File[] languagePacks = new File(languageDirectory).listFiles();
+    	for (int i = 0; i < languagePacks.length; i++) {
+    		Properties language = new Properties();
+    		try {
+				language.load(new FileInputStream(languagePacks[i]));
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+    		LanguageRegistry.instance().addStringLocalization(language, languagePacks[i].getName());
+    	}	
     }
    
     @Init
@@ -119,7 +140,6 @@ public class ElementalWands {
     	for (EnumWandElement element : EnumWandElement.values()) {
     		for (EnumWandMaterial material : EnumWandMaterial.values()) {
     			wands[wandIndex] = (ItemWand) (new ItemWand(wandStartId + wandIndex, material, element)).setUnlocalizedName("wand" + element.toString() + material.toString());
-    			LanguageRegistry.addName(wands[wandIndex], material.toString() + " " + element.toString() + " Wand");
     			wandIndex++;
     		}
     	}
@@ -127,13 +147,11 @@ public class ElementalWands {
     	// Add recipes for the wands.
     	RecipesWand.addRecipes();
     	
-    	LanguageRegistry.instance().addStringLocalization("itemGroup.wandTab", "en_US", "Wands");
-    	
         //proxy.registerRenderers();
     }
    
     @PostInit
     public void modsLoaded(FMLPostInitializationEvent event) {
-        // Stub Method
+
     }
 }
