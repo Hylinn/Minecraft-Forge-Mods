af3d91b155491e185642ae549762aa8dcbfe37c0
Hylinn <hylinn.taggart+git@gmail.com>
2013-03-17 21:43:06 -0400

    Finished the functionality for Conjure Water.

--- src/mods/hylinn/minecraft/ElementalWands/enchantment/EnchantmentConjureWater.java
+++ src/mods/hylinn/minecraft/ElementalWands/enchantment/EnchantmentConjureWater.java
@@ -1,21 +1,87 @@
 package hylinn.minecraft.ElementalWands.enchantment;
 
+import hylinn.minecraft.ElementalWands.ElementalWands;
 import hylinn.minecraft.ElementalWands.item.EnumWandElement;
+import net.minecraft.block.Block;
 import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.util.StatCollector;
+import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
 
 public class EnchantmentConjureWater extends EnchantmentWand {
 
+	private int waterType = Block.waterMoving.blockID;
+	private float rangePerLevel = 4.0F;
+	
 	public EnchantmentConjureWater(int id, int weight) {
 		super(id, weight, EnumWandElement.WATER);
 		this.setName("conjureWater");
 	}
 
 	public int cast(ItemStack stack, World world, EntityLiving entity, int level, int itemInUseDuration) {
-		System.out.println(this.getTranslatedName(level) + " cast after charging for " + itemInUseDuration);
-		return 1;
+		float seconds = (float) itemInUseDuration / 20.0F; 
+		if (ElementalWands.DEBUG) System.out.println(entity.getEntityName() + " cast " + this.getTranslatedName(level) + " after charging for " + seconds + " seconds");
+		
+		if (!world.isRemote) {
+			MovingObjectPosition target = this.getMovingObjectPositionFromEntity(world, entity, level * rangePerLevel, true);
+			
+			if (target != null) {
+				switch (target.sideHit) {
+					case 0:
+						target.blockY -= 1;
+						break;
+					case 1:
+						target.blockY += 1;
+						break;
+					case 2:
+						target.blockZ -= 1;
+						break;
+					case 3: 
+						target.blockZ += 1;
+						break;
+					case 4:
+						target.blockX -= 1;
+						break;
+					case 5: 
+						target.blockX += 1;
+						break;
+				}
+				conjureWater(world, seconds >= 1.0F ? (int) Math.min(3, (seconds + 1) / 2) : 0, target.blockX, target.blockY, target.blockZ);
+			}
+			else if (entity instanceof EntityPlayer && ((EntityPlayer) entity).capabilities.isCreativeMode) {
+				Vec3 targetAir = getPositionAtDistanceFromEntity(world, entity, level * rangePerLevel);
+				conjureWater(world, seconds >= 1.0F ? (int) Math.min(3, (seconds + 1) / 2) : 0, (int) targetAir.xCoord, (int) targetAir.yCoord, (int) targetAir.zCoord);
+			}
+		}
+		return getCastChargeCost(itemInUseDuration);
 	}
 
+	private void conjureWater(World world, int castStage, int centerX, int centerY, int centerZ) {
+		
+		if (world.provider.isHellWorld) {
+			world.playSoundEffect(centerX + 0.5D, centerY + 0.5D, centerZ + 0.5D, "random.fizz", 0.5F, 2.6F + (world.rand.nextFloat() - world.rand.nextFloat()) * 0.8F);
+
+            for (int i = 0; i < (castStage + 1) * 4; i++)
+            {
+            	double particleX = (double)centerX + Math.random();
+            	double particleY = (double)centerY + Math.random();
+            	double particleZ = (double)centerZ + Math.random();
+            	if (ElementalWands.DEBUG) System.out.println(particleX + ", " + particleY + ", " + particleZ);
+            	world.spawnParticle("largesmoke", particleX, particleY, particleZ, 0.0D, 0.0D, 0.0D);
+            }
+		}
+		else {
+			int direction = 1;
+			for (int i = 0; i <= castStage; i++) {
+				if (!world.isAirBlock(centerX, centerY + (i * direction), centerZ) && world.getBlockMaterial(centerX, centerY, centerZ).isSolid() && i > 1)
+					break;
+				else if (i <= 1)
+					direction *= -1;
+				world.setBlockAndMetadataWithNotify(centerX, centerY + (i * direction), centerZ, waterType, 0, 3);
+			}
+		}
+	}	
 }
--- src/mods/hylinn/minecraft/ElementalWands/enchantment/EnchantmentWand.java
+++ src/mods/hylinn/minecraft/ElementalWands/enchantment/EnchantmentWand.java
@@ -5,8 +5,14 @@ import hylinn.minecraft.ElementalWands.item.EnumWandElement;
 import hylinn.minecraft.ElementalWands.item.ItemWand;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnumEnchantmentType;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.MathHelper;
+import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
 
 public class EnchantmentWand extends Enchantment implements ICastable{
@@ -14,7 +20,8 @@ public class EnchantmentWand extends Enchantment implements ICastable{
 	public final EnumWandElement element;
 	protected int minLevel = 1;
 	protected int maxLevel = 5;
-	protected int castCost = 1;
+	protected float castCost = 1.0F;
+	protected float maxCastTime = 5.0F; // in Seconds
 	
 	public EnchantmentWand(int id, int weight, EnumWandElement element) {
 		super(id, weight, ElementalWands.enchantmentWand);
@@ -41,6 +48,11 @@ public class EnchantmentWand extends Enchantment implements ICastable{
 		return maxLevel;
 	}
 	
+	public int getCastChargeCost(int itemInUseDuration) {
+		float seconds = itemInUseDuration / 20.0F;
+		return (int) Math.min(maxCastTime, Math.pow(2, seconds) * castCost);
+	}
+	
 	public int getMinEnchantability(int level) {
 		return (int) Math.pow(3, level - 1);
 	}
@@ -51,6 +63,37 @@ public class EnchantmentWand extends Enchantment implements ICastable{
 	
 	public int cast(ItemStack stack, World world, EntityLiving entity, int level, int itemInUseDuration) {
 		System.out.println(this.getTranslatedName(level) + " cast after charging for " + itemInUseDuration);
-		return 0;
+		return getCastChargeCost(itemInUseDuration);
 	}
+	
+	protected Vec3 getPositionAtDistanceFromEntity(World world, Entity entity, double distance) {
+		float f1 = entity.prevRotationPitch + (entity.rotationPitch - entity.prevRotationPitch);
+        float f2 = entity.prevRotationYaw + (entity.rotationYaw - entity.prevRotationYaw);
+        
+        double entityX = entity.prevPosX + (entity.posX - entity.prevPosX);
+        double entityY = entity.prevPosY + (entity.posY - entity.prevPosY) + (( double)entity.height * 0.9D) - (double)entity.yOffset;
+        double entityZ = entity.prevPosZ + (entity.posZ - entity.prevPosZ);
+        Vec3 entityLocation = world.getWorldVec3Pool().getVecFromPool(entityX, entityY, entityZ);
+        
+        float f3 = MathHelper.cos(-f2 * 0.017453292F - (float)Math.PI);
+        float f4 = MathHelper.sin(-f2 * 0.017453292F - (float)Math.PI);
+        float f5 = -MathHelper.cos(-f1 * 0.017453292F);
+        float f6 = MathHelper.sin(-f1 * 0.017453292F);
+        float f7 = f4 * f5;
+        float f8 = f3 * f5;
+        
+        Vec3 distanceFromEntity = entityLocation.addVector((double)f7 * distance, (double)f6 * distance, (double)f8 * distance);
+		
+		return distanceFromEntity;
+	}
+	
+	protected MovingObjectPosition getMovingObjectPositionFromEntity(World world, Entity entity, double distance, boolean onlyCollidable) {
+        double entityX = entity.prevPosX + (entity.posX - entity.prevPosX);
+        double entityY = entity.prevPosY + (entity.posY - entity.prevPosY) + (( double)entity.height * 0.9D) - (double)entity.yOffset;
+        double entityZ = entity.prevPosZ + (entity.posZ - entity.prevPosZ);
+        
+        Vec3 entityLocation = world.getWorldVec3Pool().getVecFromPool(entityX, entityY, entityZ);      
+        Vec3 distanceFromEntity = getPositionAtDistanceFromEntity(world, entity, distance);
+        return world.rayTraceBlocks_do_do(entityLocation, distanceFromEntity, !onlyCollidable, onlyCollidable);
+    }
 }
