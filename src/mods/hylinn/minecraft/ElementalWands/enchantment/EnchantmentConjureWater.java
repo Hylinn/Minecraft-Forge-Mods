9d8f54dbeb4d2b9b895d7e55e5cbafaceb436a4e
Hylinn <hylinn.taggart+git@gmail.com>
2013-03-17 21:43:50 -0400

    Removed unused imports.

--- src/mods/hylinn/minecraft/ElementalWands/enchantment/EnchantmentConjureWater.java
+++ src/mods/hylinn/minecraft/ElementalWands/enchantment/EnchantmentConjureWater.java
@@ -7,7 +7,6 @@ import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.MovingObjectPosition;
-import net.minecraft.util.StatCollector;
 import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
 
